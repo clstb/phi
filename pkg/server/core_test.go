dfa55fb7646c8d0b78ea4bb027eefe75443abaab
clstb <github@clstb.codes>
2021-01-16 20:40:24 +0100

    feat: add test for GetTransactions

--- pkg/server/core_test.go
+++ pkg/server/core_test.go
@@ -2,11 +2,12 @@ package server_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/clstb/phi/pkg/pb"
 	"github.com/gofrs/uuid"
-	"github.com/golang/protobuf/ptypes"
 	"github.com/matryer/is"
 )
 
@@ -119,7 +120,7 @@ func TestCreateTransaction(t *testing.T) {
 			return client.CreateTransaction(
 				ctx,
 				&pb.Transaction{
-					Date: ptypes.TimestampNow(),
+					Date: time.Now().Format("2006-01-02"),
 					Postings: []*pb.Posting{
 						{
 							Account: account.Id,
@@ -136,6 +137,69 @@ func TestCreateTransaction(t *testing.T) {
 		check: func(t *pb.Transaction, e error) {
 			is.NoErr(e)
 			is.NoErr(db.DeleteTransaction(ctx, uuid.FromStringOrNil(t.Id)))
+		},
+	})
+
+	for _, t := range tests {
+		t.check(t.do())
+	}
+}
+
+func TestGetTransactions(t *testing.T) {
+	is := is.New(t)
+	ctx := context.Background()
+
+	client := coreClient()
+	type test struct {
+		do    func() (*pb.Transactions, error)
+		check func(*pb.Transactions, error)
+	}
+	var tests []test
+	add := func(t test) {
+		tests = append(tests, t)
+	}
+
+	account, err := client.CreateAccount(
+		ctx,
+		&pb.Account{
+			Name: "account-test",
+		},
+	)
+	is.NoErr(err)
+	defer func() {
+		is.NoErr(db.DeleteAccount(ctx, uuid.FromStringOrNil(account.Id)))
+	}()
+
+	add(test{
+		do: func() (*pb.Transactions, error) {
+			transactions, err := client.CreateTransaction(
+				ctx,
+				&pb.Transaction{
+					Date: time.Now().Format("2006-01-02"),
+					Postings: []*pb.Posting{
+						{
+							Account: account.Id,
+							Units:   "1 EUR",
+						},
+						{
+							Account: account.Id,
+							Units:   "-1 EUR",
+						},
+					},
+				},
+			)
+			fmt.Println(transactions)
+			is.NoErr(err)
+			return client.GetTransactions(
+				ctx,
+				&pb.TransactionsQuery{
+					AccountName: "^account-.*",
+				},
+			)
+		},
+		check: func(t *pb.Transactions, e error) {
+			is.NoErr(e)
+			is.NoErr(db.DeleteTransaction(ctx, uuid.FromStringOrNil(t.Data[0].Id)))
 		},
 	})
 

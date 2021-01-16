a637d81bca66ca462b1ddecdfe4db9c0962a04cf
clstb <github@clstb.codes>
2021-01-16 18:41:03 +0100

    feat: add test for GetAccounts

--- pkg/server/core_test.go
+++ pkg/server/core_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/clstb/phi/pkg/pb"
+	"github.com/gofrs/uuid"
 	"github.com/matryer/is"
 )
 
@@ -35,10 +36,50 @@ func TestCreateAccount(t *testing.T) {
 		check: func(a *pb.Account, e error) {
 			is.NoErr(e)
 			is.Equal(a.Name, "account-test")
+			is.NoErr(db.DeleteAccount(ctx, uuid.FromStringOrNil(a.Id)))
 		},
 	})
 
 	for _, t := range tests {
 		t.check(t.do())
 	}
+}
+
+func TestGetAccounts(t *testing.T) {
+	is := is.New(t)
+	ctx := context.Background()
+
+	client := coreClient()
+
+	type test struct {
+		do    func() (*pb.Accounts, error)
+		check func(*pb.Accounts, error)
+	}
+	var tests []test
+	add := func(t test) {
+		tests = append(tests, t)
+	}
+
+	add(test{
+		do: func() (*pb.Accounts, error) {
+			_, err := client.CreateAccount(
+				ctx,
+				&pb.Account{
+					Name: "account-test",
+				},
+			)
+			is.NoErr(err)
+			return client.GetAccounts(
+				ctx,
+				&pb.AccountsQuery{
+					Name: "^account-test$",
+				},
+			)
+		},
+		check: func(a *pb.Accounts, e error) {
+			is.NoErr(e)
+			is.Equal(a.Data[0].Name, "account-test")
+			is.NoErr(db.DeleteAccount(ctx, uuid.FromStringOrNil(a.Data[0].Id)))
+		},
+	})
 }

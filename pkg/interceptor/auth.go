45b87169de3b066c39e471b02c4759ad3b85eec7
clstb <github@clstb.codes>
2021-01-17 23:49:42 +0100

    feat: add auth service

---
+++ pkg/interceptor/auth.go
@@ -0,0 +1,40 @@
+package interceptor
+
+import (
+	"context"
+
+	"github.com/clstb/phi/pkg/pb"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	"google.golang.org/grpc"
+)
+
+func auth(client pb.AuthClient) func(ctx context.Context) (context.Context, error) {
+	return func(ctx context.Context) (context.Context, error) {
+		token, err := grpc_auth.AuthFromMD(ctx, "bearer")
+		if err != nil {
+			return nil, err
+		}
+
+		claims, err := client.Verify(
+			ctx,
+			&pb.JWT{
+				AccessToken: token,
+			},
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		ctx = context.WithValue(ctx, "user_id", claims.UserId)
+		ctx = context.WithValue(ctx, "user_name", claims.UserName)
+		return ctx, nil
+	}
+}
+
+func AuthUnary(client pb.AuthClient) grpc.UnaryServerInterceptor {
+	return grpc_auth.UnaryServerInterceptor(auth(client))
+}
+
+func AuthStream(client pb.AuthClient) grpc.StreamServerInterceptor {
+	return grpc_auth.StreamServerInterceptor(auth(client))
+}

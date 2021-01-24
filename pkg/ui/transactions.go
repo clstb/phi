bfd09b79b6952a2e349890a44e996b5f88dc6361
clstb <github@clstb.codes>
2021-01-25 00:07:43 +0100

    feat: implement direct feedback actions

--- pkg/ui/transactions.go
+++ pkg/ui/transactions.go
@@ -1,8 +1,11 @@
 package ui
 
 import (
+	"log"
+
 	"github.com/clstb/phi/pkg/fin"
 	"github.com/gdamore/tcell/v2"
+	"github.com/gofrs/uuid"
 	"github.com/rivo/tview"
 )
 
@@ -38,7 +41,7 @@ func (u *UI) renderTransactions() {
 			transaction.Reference,
 		}
 
-		if !transaction.Balanced() {
+		if transaction.ID == uuid.Nil {
 			color = tcell.ColorGrey
 		}
 
@@ -63,5 +66,55 @@ func (u *UI) handlerTransactions() {
 		u.renderPostings()
 		u.main.AddItem(u.side, 0, 1, false)
 		u.app.SetFocus(u.pt)
+	})
+	u.tt.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() != tcell.KeyRune {
+			return event
+		}
+
+		transaction, row := u.selectedTransaction()
+		if row == 0 {
+			return event
+		}
+
+		switch event.Rune() {
+		case 'd':
+			if transaction.ID != uuid.Nil {
+				_, err := u.core.DeleteTransaction(
+					u.ctx,
+					transaction.PB(),
+				)
+				if err != nil {
+					log.Fatal(err)
+				}
+				u.transactions = append(
+					u.transactions[:row],
+					u.transactions[row+1:]...,
+				)
+				u.tt.Select(row, 0)
+				u.render()
+			}
+			return nil
+		case 's':
+			if transaction.ID == uuid.Nil {
+				transactionPB, err := u.core.CreateTransaction(
+					u.ctx,
+					transaction.PB(),
+				)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				transaction, err := fin.TransactionFromPB(transactionPB)
+				if err != nil {
+					log.Fatal(err)
+				}
+				u.transactions[row] = transaction
+				u.render()
+			}
+			return nil
+		default:
+			return event
+		}
 	})
 }

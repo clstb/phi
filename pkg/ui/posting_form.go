f1c95ec686b7c56812728de732dd395fd588a966
clstb <github@clstb.codes>
2021-01-25 22:59:31 +0100

    feat: merge posting form preparation functions

--- pkg/ui/posting_form.go
+++ pkg/ui/posting_form.go
@@ -43,36 +43,20 @@ func (u *UI) postingFromForm() (fin.Posting, error) {
 
 }
 
-func (u *UI) pfAdd() {
-	transaction, tRow := u.selectedTransaction()
+func (u *UI) pfPrep(row int) {
+	tRow, _ := u.tt.GetSelection()
+	transaction := u.transactions[tRow-1]
 
-	u.pfia.SetText("")
-	u.pfic.SetText("")
-	u.pfip.SetText("")
-
-	u.pf.Clear(true)
-	u.pf.SetTitle("Add Posting")
-	u.pf.AddFormItem(u.pfia)
-	u.pf.AddFormItem(u.pfiu)
-	u.pf.AddFormItem(u.pfic)
-	u.pf.AddFormItem(u.pfip)
-	u.pf.AddButton("Save", func() {
-		posting, err := u.postingFromForm()
-		if err != nil {
-			return
+	var posting fin.Posting
+	if row > 0 {
+		u.pf.SetTitle("Edit Posting")
+		posting = transaction.Postings[row-1]
+	} else {
+		u.pf.SetTitle("Add Posting")
+		if len(transaction.Postings) == 1 {
+			posting.Units = transaction.Postings[0].Units.Neg()
 		}
-
-		transaction.Postings = append(transaction.Postings, posting)
-		u.transactions[tRow] = transaction
-		u.render()
-		u.side.RemoveItem(u.pf)
-		u.app.SetFocus(u.pt)
-	})
-}
-
-func (u *UI) pfEdit() {
-	transaction, tRow := u.selectedTransaction()
-	posting, pRow := u.selectedPosting()
+	}
 
 	account, ok := u.accounts.ById(posting.Account.String())
 	if !ok {
@@ -85,7 +69,6 @@ func (u *UI) pfEdit() {
 	u.pfip.SetText(posting.Price.String())
 
 	u.pf.Clear(true)
-	u.pf.SetTitle("Edit Posting")
 	u.pf.AddFormItem(u.pfia)
 	u.pf.AddFormItem(u.pfiu)
 	u.pf.AddFormItem(u.pfic)
@@ -96,14 +79,20 @@ func (u *UI) pfEdit() {
 			return
 		}
 
-		transaction.Postings[pRow] = posting
-		u.transactions[tRow] = transaction
+		if row > 0 {
+			transaction.Postings = append(transaction.Postings, posting)
+		} else {
+			transaction.Postings[row-1] = posting
+		}
 
-		u.render()
+		u.transactions[tRow-1] = transaction
+
+		u.renderTransactions()
+		u.renderPostings(transaction)
+
 		u.side.RemoveItem(u.pf)
 		u.app.SetFocus(u.pt)
 	})
-
 }
 
 func (u *UI) handlerPostingForm() {

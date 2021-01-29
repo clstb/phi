9071a36479e1fedbbf5243dd8cda55c31ad2cdba
clstb <github@clstb.codes>
2021-01-29 21:52:02 +0100

    feat: add package level doc

---
+++ pkg/fin/doc.go
@@ -0,0 +1,4 @@
+// Package fin provides business logic on top of phi's entity models.
+// It holds all code for internal financial calculations.
+// For relevant structs (un)marshallers for their corresponding protobuf and database spec are implemented.
+package fin

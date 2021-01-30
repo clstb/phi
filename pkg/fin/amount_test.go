2f012be2b695543cc8316f4c03f7a5ddf68135c8
clstb <github@clstb.codes>
2021-01-30 12:52:00 +0100

    feat: add tests for amount add and mul

--- pkg/fin/amount_test.go
+++ pkg/fin/amount_test.go
@@ -79,3 +79,119 @@ func TestAmountFromString(t *testing.T) {
 		t.check(t.do())
 	}
 }
+
+func TestAmountAdd(t *testing.T) {
+	is := is.New(t)
+
+	type test struct {
+		do    func() (fin.Amount, error)
+		check func(fin.Amount, error)
+	}
+	var tests []test
+	add := func(t test) {
+		tests = append(tests, t)
+	}
+	fromString := func(s string) fin.Amount {
+		amount, err := fin.AmountFromString(s)
+		is.NoErr(err)
+		return amount
+	}
+
+	add(test{
+		do: func() (fin.Amount, error) {
+			return fromString("1 EUR").Add(fromString("1 EUR"))
+		},
+		check: func(a fin.Amount, e error) {
+			is.NoErr(e)
+			is.Equal(a.String(), fromString("2 EUR").String())
+		},
+	})
+	add(test{
+		do: func() (fin.Amount, error) {
+			return fromString("0.1 EUR").Add(fromString("0.1 EUR"))
+		},
+		check: func(a fin.Amount, e error) {
+			is.NoErr(e)
+			is.Equal(a.String(), fromString("0.2 EUR").String())
+		},
+	})
+	add(test{
+		do: func() (fin.Amount, error) {
+			return fromString("0.1 EUR").Add(fromString("0 EUR"))
+		},
+		check: func(a fin.Amount, e error) {
+			is.NoErr(e)
+			is.Equal(a.String(), fromString("0.1 EUR").String())
+		},
+	})
+	add(test{
+		do: func() (fin.Amount, error) {
+			return fromString("1 EUR").Add(fromString("1 CAD"))
+		},
+		check: func(a fin.Amount, e error) {
+			is.Equal(e, fin.ErrMismatchedCurrency)
+		},
+	})
+
+	for _, t := range tests {
+		t.check(t.do())
+	}
+}
+
+func TestAmountMul(t *testing.T) {
+	is := is.New(t)
+
+	type test struct {
+		do    func() (fin.Amount, error)
+		check func(fin.Amount, error)
+	}
+	var tests []test
+	add := func(t test) {
+		tests = append(tests, t)
+	}
+	fromString := func(s string) fin.Amount {
+		amount, err := fin.AmountFromString(s)
+		is.NoErr(err)
+		return amount
+	}
+
+	add(test{
+		do: func() (fin.Amount, error) {
+			return fromString("2 EUR").Mul(fromString("5 EUR"))
+		},
+		check: func(a fin.Amount, e error) {
+			is.NoErr(e)
+			is.Equal(a.String(), fromString("10 EUR").String())
+		},
+	})
+	add(test{
+		do: func() (fin.Amount, error) {
+			return fromString("0.1 EUR").Mul(fromString("10 EUR"))
+		},
+		check: func(a fin.Amount, e error) {
+			is.NoErr(e)
+			is.Equal(a.String(), fromString("1 EUR").String())
+		},
+	})
+	add(test{
+		do: func() (fin.Amount, error) {
+			return fromString("0.1 EUR").Mul(fromString("0 EUR"))
+		},
+		check: func(a fin.Amount, e error) {
+			is.NoErr(e)
+			is.Equal(a.String(), fromString("0 EUR").String())
+		},
+	})
+	add(test{
+		do: func() (fin.Amount, error) {
+			return fromString("2 EUR").Mul(fromString("5 CAD"))
+		},
+		check: func(a fin.Amount, e error) {
+			is.Equal(e, fin.ErrMismatchedCurrency)
+		},
+	})
+
+	for _, t := range tests {
+		t.check(t.do())
+	}
+}

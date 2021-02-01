8426b66c066f83e332f3b2291d8b77b1fc2c3a5b
clstb <github@clstb.codes>
2021-02-01 23:01:11 +0100

    feat: write simple accounts integration test

--- pkg/core/accounts_test.go
+++ pkg/core/accounts_test.go
@@ -2,8 +2,11 @@ package core_test
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 
+	"github.com/clstb/phi/pkg/core"
+	db "github.com/clstb/phi/pkg/db/core"
 	"github.com/clstb/phi/pkg/pb"
 	"github.com/gofrs/uuid"
 	"github.com/matryer/is"
@@ -11,79 +14,118 @@ import (
 
 func TestCreateAccount(t *testing.T) {
 	is := is.New(t)
-	ctx := context.Background()
 
-	client := client()
+	server := core.New(core.WithDB(sqlDB))
 
 	type test struct {
-		do    func() (*pb.Account, error)
-		check func(*pb.Account, error)
+		do    func() (*sql.Tx, *pb.Account, error)
+		check func(*sql.Tx, *pb.Account, error) *sql.Tx
 	}
 	var tests []test
 	add := func(t test) {
 		tests = append(tests, t)
 	}
+	tx := func() (*sql.Tx, context.Context) {
+		tx, err := sqlDB.Begin()
+		is.NoErr(err)
+		ctx := context.WithValue(
+			context.Background(),
+			"tx",
+			tx,
+		)
+
+		return tx, context.WithValue(
+			ctx,
+			"sub",
+			uuid.Nil.String(),
+		)
+	}
 
 	add(test{
-		do: func() (*pb.Account, error) {
-			return client.CreateAccount(
+		do: func() (*sql.Tx, *pb.Account, error) {
+			tx, ctx := tx()
+			res, err := server.CreateAccount(
 				ctx,
 				&pb.Account{
 					Name: "account-test",
 				},
 			)
+			return tx, res, err
 		},
-		check: func(a *pb.Account, e error) {
+		check: func(tx *sql.Tx, a *pb.Account, e error) *sql.Tx {
 			is.NoErr(e)
 			is.Equal(a.Name, "account-test")
-			is.NoErr(db.DeleteAccount(ctx, uuid.FromStringOrNil(a.Id)))
+			q := db.New(tx)
+			accounts, err := q.GetAccounts(context.Background(), db.GetAccountsParams{
+				Name: "account-test",
+			})
+			is.NoErr(err)
+			is.Equal(len(accounts), 1)
+			return tx
 		},
 	})
 
 	for _, t := range tests {
-		t.check(t.do())
+		is.NoErr(t.check(t.do()).Rollback())
 	}
 }
 
 func TestGetAccounts(t *testing.T) {
 	is := is.New(t)
-	ctx := context.Background()
 
-	client := client()
+	server := core.New(core.WithDB(sqlDB))
 
 	type test struct {
-		do    func() (*pb.Accounts, error)
-		check func(*pb.Accounts, error)
+		do    func() (*sql.Tx, *pb.Accounts, error)
+		check func(*sql.Tx, *pb.Accounts, error) *sql.Tx
 	}
 	var tests []test
 	add := func(t test) {
 		tests = append(tests, t)
 	}
+	tx := func() (*sql.Tx, context.Context) {
+		tx, err := sqlDB.Begin()
+		is.NoErr(err)
+
+		ctx := context.WithValue(
+			context.Background(),
+			"tx",
+			tx,
+		)
+
+		return tx, context.WithValue(
+			ctx,
+			"sub",
+			uuid.Nil.String(),
+		)
+	}
 
 	add(test{
-		do: func() (*pb.Accounts, error) {
-			_, err := client.CreateAccount(
-				ctx,
-				&pb.Account{
-					Name: "account-test",
-				},
-			)
+		do: func() (*sql.Tx, *pb.Accounts, error) {
+			tx, ctx := tx()
+			_, err := server.CreateAccount(ctx, &pb.Account{
+				Name: "account-test",
+			})
 			is.NoErr(err)
-			return client.GetAccounts(
+
+			res, err := server.GetAccounts(
 				ctx,
 				&pb.AccountsQuery{
 					Name: "^account-test$",
 				},
 			)
+
+			return tx, res, err
 		},
-		check: func(a *pb.Accounts, e error) {
+		check: func(tx *sql.Tx, a *pb.Accounts, e error) *sql.Tx {
 			is.NoErr(e)
+			is.Equal(len(a.Data), 1)
 			is.Equal(a.Data[0].Name, "account-test")
-			is.NoErr(db.DeleteAccount(ctx, uuid.FromStringOrNil(a.Data[0].Id)))
+			return tx
 		},
 	})
 
 	for _, t := range tests {
-		t.check(t.do())
+		is.NoErr(t.check(t.do()).Rollback())
 	}
 }
--- pkg/core/server_test.go
+++ pkg/core/server_test.go
@@ -3,71 +3,29 @@ package core_test
 import (
 	"database/sql"
 	"log"
-	"net"
 	"os"
 	"testing"
-	"time"
 
-	"github.com/clstb/phi/pkg/core"
-	coredb "github.com/clstb/phi/pkg/db/core"
-	"github.com/clstb/phi/pkg/pb"
 	_ "github.com/lib/pq"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/test/bufconn"
 )
 
-const bufSize = 1024 * 1024
-
-var conn *grpc.ClientConn
-var db *coredb.Queries
-
-func client() pb.CoreClient {
-	return pb.NewCoreClient(conn)
-}
+var sqlDB *sql.DB
 
 func TestMain(m *testing.M) {
-	sqlDB, err := sql.Open(
+	var err error
+	sqlDB, err = sql.Open(
 		"postgres",
-		"postgres://phi@127.0.0.1:26257/phi?sslmode=disable",
+		"postgresql://root@localhost:26257?sslmode=disable",
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer sqlDB.Close()
+
 	if err := sqlDB.Ping(); err != nil {
 		log.Fatal(err)
 	}
-	db = coredb.New(sqlDB)
-
-	lis := bufconn.Listen(bufSize)
-	s := grpc.NewServer()
-	server := core.New(
-		core.WithDB(sqlDB),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	pb.RegisterCoreServer(s, server)
-
-	go func() {
-		if err := s.Serve(lis); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	conn, err = grpc.Dial(
-		"bufnet",
-		grpc.WithInsecure(),
-		grpc.WithDialer(func(s string, d time.Duration) (net.Conn, error) {
-			return lis.Dial()
-		}),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
 
 	status := m.Run()
-	s.GracefulStop()
 	os.Exit(status)
 }

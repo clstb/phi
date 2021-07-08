96f3e3793e62c554dbaa00c44a456c300b96b262
clstb <contact@clstb.codes>
2021-07-08 15:26:17 +0200

    feat: implement categorize command

--- cmd/categorize.go
+++ cmd/categorize.go
@@ -4,10 +4,9 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/c-bata/go-prompt"
 	"github.com/clstb/phi/pkg/fin"
 	"github.com/clstb/phi/pkg/pb"
-	"github.com/lithammer/fuzzysearch/fuzzy"
-	"github.com/manifoldco/promptui"
 	"github.com/navossoc/bayesian"
 	"github.com/urfave/cli/v2"
 )
@@ -44,28 +43,71 @@ func Categorize(ctx *cli.Context) error {
 	if err != nil {
 		return err
 	}
+	categorized := fin.Transactions{}
 
 	classifier, err := bayesian.NewClassifierFromFile("./phi.classifier")
 	if err != nil {
 		return err
 	}
-	classes := classifier.Classes
 
-	categorized := fin.Transactions{}
+	suggestions := []prompt.Suggest{
+		{Text: "Done", Description: "Push transactions and exit"},
+		{Text: "Add", Description: "Add new account"},
+		{Text: "Skip", Description: "Skip this transaction"},
+	}
+	for _, v := range accountsById {
+		suggestions = append(suggestions, prompt.Suggest{
+			Text:        v.Name,
+			Description: v.ID.String(),
+		})
+	}
+
+	completer := func(in prompt.Document) []prompt.Suggest {
+		w := in.GetWordBeforeCursor()
+		if w == "" {
+			return []prompt.Suggest{}
+		}
+		return prompt.FilterFuzzy(suggestions, w, true)
+	}
+
+	executor := func(in string) {
+		in = strings.TrimSpace(in)
+		fmt.Println(in)
+	}
+
+	p := prompt.New(
+		executor,
+		completer,
+		prompt.OptionSuggestionTextColor(prompt.White),
+		prompt.OptionSuggestionBGColor(prompt.Black),
+		prompt.OptionDescriptionTextColor(prompt.White),
+		prompt.OptionDescriptionBGColor(prompt.DarkGray),
+		prompt.OptionSelectedSuggestionTextColor(prompt.DarkGreen),
+		prompt.OptionSelectedSuggestionBGColor(prompt.Black),
+		prompt.OptionSelectedDescriptionTextColor(prompt.White),
+		prompt.OptionSelectedDescriptionBGColor(prompt.DarkGray),
+	)
+
 L:
-	for _, transaction := range transactions {
+	for i := 0; i < len(transactions); {
+		transaction := transactions[i]
+
 		_, inx, _ := classifier.LogScores(strings.Split(
 			transaction.Entity,
 			" ",
 		))
-		class := strings.Split(string(classes[inx]), " ")
-		fromID, toID := class[0], class[1]
+		class := strings.Split(string(classifier.Classes[inx]), " ")
+
+		from, to := accountsById[class[0]], accountsById[class[1]]
+		transaction.From = from.ID
+		transaction.To = to.ID
 
 		if transaction.Debit {
 			transaction.Units = transaction.Units.Neg()
 			transaction.Cost = transaction.Cost.Neg()
 			transaction.Price = transaction.Price.Neg()
 		}
+
 		fmt.Printf(
 			"%s\n%s [%s] (%s) %s\n\n",
 			transaction.Entity,
@@ -74,112 +116,70 @@ L:
 			transaction.Price.Color(false),
 			fmt.Sprintf(
 				"%s => %s",
-				accountsById[fromID].Name,
-				accountsById[toID].Name,
+				from.Name,
+				to.Name,
 			),
 		)
 
-		var fromName string
-		for {
-			accountNames := append([]string{"Add account", "Done"}, accounts.Names()...)
-			pSelect := promptui.Select{
-				Label:             "From",
-				Items:             accountNames,
-				StartInSearchMode: true,
-				Searcher: func(s string, i int) bool {
-					s = strings.ToLower(strings.TrimSpace(s))
-					name := strings.ToLower(strings.TrimSpace(accountNames[i]))
-					return fuzzy.Match(s, name)
-				},
+		in := p.Input()
+		in = strings.TrimSpace(in)
+		blocks := strings.Split(in, " ")
+
+		switch {
+		case blocks[0] == "Add":
+			if len(blocks) != 2 {
+				fmt.Printf("Please specify an account name!\n")
+				continue
 			}
-			_, fromName, err = pSelect.Run()
+
+			accountPB, err := core.CreateAccount(
+				ctx.Context,
+				&pb.Account{
+					Name: blocks[1],
+				},
+			)
+			if err != nil {
+				return err
+			}
+			account, err := fin.AccountFromPB(accountPB)
 			if err != nil {
 				return err
 			}
 
-			if fromName == "Add account" {
-				p := promptui.Prompt{
-					Label: "Account name",
-				}
-				accountName, err := p.Run()
-				if err != nil {
-					return err
-				}
-
-				accountPB, err := core.CreateAccount(
-					ctx.Context,
-					&pb.Account{
-						Name: accountName,
-					},
-				)
-				if err != nil {
-					return err
-				}
-				account, err := fin.AccountFromPB(accountPB)
-				if err != nil {
-					return err
-				}
-
-				accounts = append(accounts, account)
-			} else if fromName == "Done" {
-				break L
-			} else {
+			accounts = append(accounts, account)
+			suggestions = append(suggestions, prompt.Suggest{
+				Text:        account.Name,
+				Description: account.ID.String(),
+			})
+		case blocks[0] == "Done":
+			break L
+		case blocks[0] == "Exit":
+			return nil
+		case blocks[0] == "Skip":
+			i++
+		case blocks[0] == "":
+			categorized = append(categorized, transaction)
+			i++
+		default:
+			if len(blocks) != 2 {
+				fmt.Printf("Please specify two account names!\n")
 				break
 			}
-		}
 
-		var toName string
-		for {
-			accountNames := append([]string{"Add account", "Done"}, accounts.Names()...)
-			pSelect := promptui.Select{
-				Label:             "To",
-				Items:             accountNames,
-				StartInSearchMode: true,
-				Searcher: func(s string, i int) bool {
-					s = strings.ToLower(strings.TrimSpace(s))
-					name := strings.ToLower(strings.TrimSpace(accountNames[i]))
-					return fuzzy.Match(s, name)
-				},
-			}
-			_, toName, err = pSelect.Run()
-			if err != nil {
-				return err
-			}
-
-			if toName == "Add account" {
-				p := promptui.Prompt{
-					Label: "Account name",
-				}
-				accountName, err := p.Run()
-				if err != nil {
-					return err
-				}
-
-				accountPB, err := core.CreateAccount(
-					ctx.Context,
-					&pb.Account{
-						Name: accountName,
-					},
-				)
-				if err != nil {
-					return err
-				}
-				account, err := fin.AccountFromPB(accountPB)
-				if err != nil {
-					return err
-				}
-
-				accounts = append(accounts, account)
-			} else if toName == "Done" {
-				break L
-			} else {
+			from, to := accounts.ByName(blocks[0]), accounts.ByName(blocks[1])
+			if from.Empty() {
+				fmt.Printf("Account not found: %s\n", blocks[0])
 				break
 			}
-		}
+			if to.Empty() {
+				fmt.Printf("Account not found: %s\n", blocks[1])
+				break
+			}
 
-		transaction.From = accounts.ByName(fromName).ID
-		transaction.To = accounts.ByName(toName).ID
-		categorized = append(categorized, transaction)
+			transaction.From, transaction.To = from.ID, to.ID
+			categorized = append(categorized, transaction)
+			i++
+		}
 	}
 
 	_, err = core.UpdateTransactions(
